d76bb187129a4fd4f93d20b9f33dcec97ce9ea6a
Alex Hartchenko <aj.tomsk@mail.ru>
2022-05-04 16:29:40 +0700

    MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,14 +1,15 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.post.Post
-import kotlin.math.log10
-import kotlin.math.pow
-import kotlin.math.roundToInt
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel = PostViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,70 +17,64 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась" +
-                    " с интенсивов по онлайн-маркетингу. Затем появились курсы по " +
-                    "дизайну, разработке, аналитике и управлению. Мы растём сами и " +
-                    "помогаем расти студентам: от новичков до уверенных профессионалов." +
-                    " Но самое важное остаётся с нами: мы верим, что в каждом уже есть" +
-                    " сила, которая заставляет хотеть больше, целиться выше, бежать быстрее." +
-                    " Наша миссия — помочь встать на путь роста и начать цепочку перемен" +
-                    " → http://netolo.gy/fyb",
-            published = "28 апреля 2022г. ",
-            likes = 999,
-            shared = 96,
-            viewCount = 990,
-            likedByMe = false
-        )
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            if (post.likedByMe) {
-                like.setImageResource(R.drawable.ic_like_24)
-            }
-            likes.text = format(post.likes)
-            shared.text = format(post.shared)
-            viewCount.text = format(post.viewCount)
-
-            like.setOnClickListener {
-                post.likedByMe = !post.likedByMe
-                like.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
-                )
-                if (post.likedByMe) post.likes++ else post.likes--
-                likes.text = format(post.likes)
-            }
-
-            shares.setOnClickListener {
-                shared.text = format(post.shared++)
-            }
-
-            views.setOnClickListener {
-                viewCount.text = format(post.viewCount++)
-            }
-
+        viewModel.data.observe(this) { post ->
+            binding.render(post)
         }
+
+        binding.like.setOnClickListener {
+            viewModel.onLikeClicked()
+        }
+
+        binding.shares.setOnClickListener {
+            viewModel.onSharedClicked()
+        }
+
+        binding.views.setOnClickListener {
+            viewModel.onViewsClicked()
+        }
+
     }
 
-    private fun format(number: Int): String {
-        val suffix = arrayOf("k", "m", "b", "t")
-        var size = if (number != 0) log10(number.toDouble())
-            .toInt() else 0
-        if (size >= 3) {
-            while (size % 3 != 0) {
-                size -= 1
-            }
-        }
-        val notation = 10.0.pow(size.toDouble())
-        return if (size >= 3) (((number / notation * 100).roundToInt() / 100.0).toString() + suffix[size / 3 - 1]) else number.toString() + ""
+    private fun ActivityMainBinding.render(post: Post) {
+        likes.text = getFormattedNumber(post.likes)
+        shared.text = getFormattedNumber(post.shared)
+        viewCount.text = getFormattedNumber(post.viewCount)
+        like.setImageResource(getButtonLikesIconResId(post.likedByMe))
     }
 
+    @DrawableRes
+    private fun getButtonLikesIconResId(liked: Boolean) =
+        if (liked) R.drawable.ic_liked_24 else R.drawable.ic_like_24
 
+    private fun getFormattedNumber(number: Int): String {
+        return when (number) {
+            0 -> ""
+            in 1..999 -> String.format(getString(R.string.numberOnes), number.toFloat())
+            in 1_000..1_099 -> String.format(
+                getString(R.string.numberThousands),
+                kotlin.math.floor(number.toDouble() / 100) / 10
+            )
+            in 1_100..9_999 -> String.format(
+                getString(R.string.numberThousandsAndHundreds),
+                kotlin.math.floor(number.toDouble() / 100) / 10
+            )
+            in 10_000..999_999 -> String.format(
+                getString(R.string.numberThousands),
+                kotlin.math.floor(number.toDouble() / 100) / 10
+            )
+            in 1_000_000..1_099_000 -> String.format(
+                getString(R.string.numberMillions),
+                kotlin.math.floor(number.toDouble() / 100_000) / 10
+            )
+            in 1_100_000..9_999_999 -> String.format(
+                getString(R.string.numberMillionsAndThousands),
+                kotlin.math.floor(number.toDouble() / 100_000) / 10
+            )
+            else -> String.format(
+                getString(R.string.numberMillions),
+                kotlin.math.floor(number.toDouble() / 100_000) / 10
+            )
+        }
 
+    }
 }
-
-
-
---
+++ app/src/main/java/ru/netology/nmedia/data/impl/InMemoryPostRepository.kt
@@ -0,0 +1,52 @@
+package ru.netology.nmedia.data.impl
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.data.PostRepository
+import ru.netology.nmedia.dto.Post
+
+
+class InMemoryPostRepository : PostRepository {
+
+    override val data = MutableLiveData(
+        Post(
+            id = 0L,
+            likes = 999,
+            shared = 970,
+            viewCount = 960
+        )
+    )
+
+    override fun like() {
+        val currentPost = checkNotNull(data.value) {
+            "Data value should not be null"
+        }
+
+        val modifiedPost = currentPost.copy(
+            likedByMe = !currentPost.likedByMe,
+            likes = currentPost.likes + if (!currentPost.likedByMe) 1 else -1
+        )
+        data.value = modifiedPost
+    }
+
+    override fun repost() {
+        val currentPost = checkNotNull(data.value) {
+            "Data value should not be null"
+        }
+        val modifiedPost = currentPost.copy(
+            shared = currentPost.shared + 10
+        )
+        data.value = modifiedPost
+    }
+
+    override fun views() {
+        val currentPost = checkNotNull(data.value) {
+            "Data value should not be null"
+        }
+        val modifiedPost = currentPost.copy(
+            viewCount = currentPost.viewCount + 10
+        )
+        data.value = modifiedPost
+    }
+
+}
