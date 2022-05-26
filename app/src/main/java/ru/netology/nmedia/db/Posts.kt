bacfc4f7958970742203b395887ceee25b10ba74
Alex Hartchenko <aj.tomsk@mail.ru>
2022-05-26 21:51:05 +0700

    SQLite)))

---
+++ app/src/main/java/ru/netology/nmedia/db/Posts.kt
@@ -0,0 +1,17 @@
+package ru.netology.nmedia.db
+
+import android.database.Cursor
+import ru.netology.nmedia.dto.Post
+
+fun Cursor.toPost() = Post(
+    id = getLong(getColumnIndexOrThrow(PostsTable.Column.ID.columnName)),
+    author = getString(getColumnIndexOrThrow(PostsTable.Column.AUTHOR.columnName)),
+    content = getString(getColumnIndexOrThrow(PostsTable.Column.CONTENT.columnName)),
+    published = getString(getColumnIndexOrThrow(PostsTable.Column.PUBLISHED.columnName)),
+    likes = getInt(getColumnIndexOrThrow(PostsTable.Column.LIKES.columnName)),
+    likedByMe = getInt(getColumnIndexOrThrow(PostsTable.Column.LIKED_BY_ME.columnName)) != 0,
+    reposts = getInt(getColumnIndexOrThrow(PostsTable.Column.REPOSTS.columnName)),
+    views = getInt(getColumnIndexOrThrow(PostsTable.Column.VIEWS.columnName)),
+    videoURL = getString(getColumnIndexOrThrow(PostsTable.Column.VIDEO_URL.columnName)),
+
+    )
--- app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -8,7 +8,9 @@ import ru.netology.nmedia.adapter.PostInteractionListener
 import ru.netology.nmedia.data.PostRepository
 import ru.netology.nmedia.data.impl.FilePostRepository
 import ru.netology.nmedia.data.impl.InMemoryPostRepository
+import ru.netology.nmedia.data.impl.SQLiteRepository
 import ru.netology.nmedia.data.impl.SharedPrefsPostRepository
+import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.util.SingleLiveEvent
 import java.text.SimpleDateFormat
@@ -19,7 +21,11 @@ class PostViewModel(
 ) : AndroidViewModel(application), PostInteractionListener {
 
     private val repository: PostRepository =
-        FilePostRepository(application)
+        SQLiteRepository(
+            dao = AppDb.getInstance(
+                context = application
+            ).postDao
+        )
 
     val data by repository::data
 
