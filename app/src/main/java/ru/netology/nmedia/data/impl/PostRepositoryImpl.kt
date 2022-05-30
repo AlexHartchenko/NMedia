454904f7f4109e9c0048e20fb5b60dbfb7b47929
Alex Hartchenko <aj.tomsk@mail.ru>
2022-05-30 20:39:58 +0700

    Notifications_Pushes

--- app/src/main/java/ru/netology/nmedia/data/impl/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/data/impl/PostRepositoryImpl.kt
@@ -2,6 +2,7 @@ package ru.netology.nmedia.data.impl
 
 import androidx.lifecycle.map
 import ru.netology.nmedia.data.PostRepository
+import ru.netology.nmedia.db.PostEntity
 import ru.netology.nmedia.db.PostsDao
 import ru.netology.nmedia.db.toEntity
 import ru.netology.nmedia.db.toModel
@@ -16,7 +17,8 @@ class PostRepositoryImpl(
     }
 
     override fun save(post: Post) {
-        dao.save(post.toEntity())
+       if (post.id == PostRepository.NEW_POST_ID) dao.insert(post.toEntity())
+       else dao.updateContentById(post.id, post.content)
     }
 
     override fun like(postId: Long) {
